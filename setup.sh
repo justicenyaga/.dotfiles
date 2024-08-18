db6117e48ff2db4091874fcb928079f4d14a06f5
Justice Nyaga <justiceevansmwema@gmail.com>
2024-08-18 15:25:22 +0300

    Add mpd and ncmpcpp configs

--- setup.sh
+++ setup.sh
@@ -33,12 +33,12 @@ sleep 1
 
 packages="apple_cursor base bat blueman brave-bin brightnessctl chili-sddm-theme eza fd feh 
   fzf git-credential-manager-bin git-delta grim gvfs gvfs-mtp htop hyprland hyprlock hyprpaper 
-  i3-wm keyd kitty kvantum lazygit man-db mercurial mpv neovim net-tools ntfs-3g nwg-look pass 
-  picom polybar power-profiles-daemon python-pipenv python-pipx python-pynvim qt6ct ripgrep rofi 
-  shotwell stow thunar thunar-volman tmux ttf-cascadia-code ttf-fira-sans ttf-jetbrains-mono-nerd 
-  ttf-joypixels ttf-meslo-nerd ttf-nerd-fonts-symbols-mono ttf-roboto ttf-terminus-nerd unzip 
-  waybar wget wlogout wl-clipboard wofi xclip xdg-user-dirs xfce4-notifyd xfce-polkit xorg-font-util 
-  xorg-fonts-misc xorg-mkfontscale yazi zip zoxide zsh"
+  i3-wm keyd kitty kvantum lazygit man-db mercurial mpd mpc mpv neovim net-tools ntfs-3g nwg-look 
+  pass picom polybar power-profiles-daemon python-pipenv python-pipx python-pynvim qt6ct ripgrep 
+  rofi shotwell stow thunar thunar-volman timidity++ tmux ttf-cascadia-code ttf-fira-sans 
+  ttf-jetbrains-mono-nerd ttf-joypixels ttf-meslo-nerd ttf-nerd-fonts-symbols-mono ttf-roboto 
+  ttf-terminus-nerd unzip waybar wget wlogout wl-clipboard wofi xclip xdg-user-dirs xfce4-notifyd 
+  xfce-polkit xorg-font-util xorg-fonts-misc xorg-mkfontscale yazi zip zoxide zsh"
 
 yay -Sy $packages --needed --noconfirm
 
@@ -48,6 +48,7 @@ yay -Sy $packages --needed --noconfirm
 if [[ ! -e "$HOME/.config/user-dirs.dirs" ]]; then
   xdg-user-dirs-update
 fi
+mkdir ~/.mpd
 
 #### INSTALL RICE ####
 
@@ -57,8 +58,8 @@ sleep 1
 # install root target packages
 sudo stow --adopt -t / keyd sddm sddm-Xsetup
 
-home_target="bat fonts git hypr i3 kitty kvantum kwalletrc lazygit nvim picom polybar rofi tmux 
-  wallpapers waybar wlogout wofi xfce4 zsh"
+home_target="bat fonts git hypr i3 kitty kvantum kwalletrc lazygit mpd ncmpcpp nvim picom polybar rofi 
+  tmux wallpapers waybar wlogout wofi xfce4 zsh"
 
 # install home target packages
 stow --adopt -t ~/ $home_target
