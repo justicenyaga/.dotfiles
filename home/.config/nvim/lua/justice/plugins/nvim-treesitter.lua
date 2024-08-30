b21f243e3fbb9b06d65a065c9d7721ab22bd4296
Justice Nyaga <justiceevansmwema@gmail.com>
2024-08-30 11:54:09 +0300

    Add setup for nvim-ts-autotag

--- home/.config/nvim/lua/justice/plugins/nvim-treesitter.lua
+++ home/.config/nvim/lua/justice/plugins/nvim-treesitter.lua
@@ -1,64 +1,88 @@
 return {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    event = { "BufReadPre", "BufNewFile" },
-    build = ":TSUpdate",
-    dependencies = {
-      "nvim-treesitter/nvim-treesitter-textobjects",
-      "windwp/nvim-ts-autotag",
-    },
-    config = function()
-      -- import nvim-treesitter plugin
-      local treesitter = require("nvim-treesitter.configs")
+	{
+		"nvim-treesitter/nvim-treesitter",
+		event = { "BufReadPre", "BufNewFile" },
+		build = ":TSUpdate",
+		dependencies = {
+			"nvim-treesitter/nvim-treesitter-textobjects",
+			"windwp/nvim-ts-autotag",
+		},
+		config = function()
+			-- import nvim-treesitter plugin
+			local treesitter = require("nvim-treesitter.configs")
 
-      -- configure treesitter
-      treesitter.setup({ -- enable syntax highlighting
-        highlight = {
-          enable = true,
-        },
-        -- enable indentation
-        indent = { enable = true },
-        -- enable autotagging (w/ nvim-ts-autotag plugin)
-        autotag = {
-          enable = true,
-        },
-        -- ensure these language parsers are installed
-        ensure_installed = {
-          "bash",
-          "c",
-          "cpp",
-          "css",
-          "dockerfile",
-          "gitignore",
-          "go",
-          "html",
-          "java",
-          "javascript",
-          "json",
-          "lua",
-          "markdown",
-          "markdown_inline",
-          "python",
-          "regex",
-          "tsx",
-          "typescript",
-          "vim",
-          "vimdoc",
-          "yaml",
-        },
-        incremental_selection = {
-          enable = true,
-          keymaps = {
-            init_selection = "<Enter>",
-            node_incremental = "<Enter>",
-            scope_incremental = false,
-            node_decremental = "<bs>",
-          },
-        },
-      })
+			require("nvim-ts-autotag").setup({
+				opts = {
+					-- Defaults
+					enable_close = true, -- Auto close tags
+					enable_rename = true, -- Auto rename pairs of tags
+					enable_close_on_slash = false, -- Auto close on trailing </
+				},
+			})
 
-      -- enable nvim-ts-context-commentstring plugin for commenting tsx and jsx
-      require("ts_context_commentstring").setup({})
-    end,
-  },
+			-- configure treesitter
+			treesitter.setup({ -- enable syntax highlighting
+				highlight = {
+					enable = true,
+				},
+				-- enable indentation
+				indent = { enable = true },
+				-- enable autotagging (w/ nvim-ts-autotag plugin)
+				autotag = {
+					enable = true,
+					filetypes = {
+						"html",
+						"javascript",
+						"typescript",
+						"typescript.glimmer",
+						"javascript.glimmer",
+						"javascriptreact",
+						"typescriptreact",
+						"markdown",
+						"glimmer",
+						"handlebars",
+						"hbs",
+						"svelte",
+						"vue",
+					},
+				},
+				-- ensure these language parsers are installed
+				ensure_installed = {
+					"bash",
+					"c",
+					"cpp",
+					"css",
+					"dockerfile",
+					"gitignore",
+					"go",
+					"html",
+					"java",
+					"javascript",
+					"json",
+					"lua",
+					"markdown",
+					"markdown_inline",
+					"python",
+					"regex",
+					"tsx",
+					"typescript",
+					"vim",
+					"vimdoc",
+					"yaml",
+				},
+				incremental_selection = {
+					enable = true,
+					keymaps = {
+						init_selection = "<Enter>",
+						node_incremental = "<Enter>",
+						scope_incremental = false,
+						node_decremental = "<bs>",
+					},
+				},
+			})
+
+			-- enable nvim-ts-context-commentstring plugin for commenting tsx and jsx
+			require("ts_context_commentstring").setup({})
+		end,
+	},
 }
