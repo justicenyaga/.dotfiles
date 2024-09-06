de383b18d19888291e18fd4dfc0aacd5082ae612
Justice Nyaga <justiceevansmwema@gmail.com>
2024-09-06 08:40:37 +0300

    Minimize vim-dadbod-ui notifications

--- home/.config/nvim/lua/justice/plugins/vim-dadbod-ui.lua
+++ home/.config/nvim/lua/justice/plugins/vim-dadbod-ui.lua
@@ -14,8 +14,8 @@ return {
 
 		vim.g.db_ui_use_nerd_fonts = 1
 		vim.g.db_ui_show_database_icon = 1
-		vim.g.db_ui_use_nvim_notify = 1
-		vim.g.db_ui_winwidth = 35
+		vim.g.db_ui_force_echo_notifications = 1
+		vim.g.db_ui_winwidth = 32
 
 		local open_in_current_tab = function()
 			local open = false
