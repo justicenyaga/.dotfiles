32de53f8d92c49d1886b472f053343bc99f8159e
Justice Nyaga <justiceevansmwema@gmail.com>
2024-09-12 15:12:37 +0300

    Add image rendering support on nvim

---
+++ home/.config/nvim/lua/justice/plugins/image-nvim.lua
@@ -0,0 +1,42 @@
+return {
+	{
+		"vhyrro/luarocks.nvim",
+		priority = 1001,
+		opts = {
+			rocks = { "magick" },
+		},
+	},
+	{
+		"3rd/image.nvim",
+		dependencies = { "luarocks.nvim" },
+		config = function()
+			require("image").setup({
+				integrations = {
+					markdown = {
+						enabled = true,
+						clear_in_insert_mode = false,
+						download_remote_images = true,
+						only_render_image_at_cursor = true,
+						filetypes = { "markdown", "vimwiki" },
+					},
+					neorg = {
+						enabled = true,
+						clear_in_insert_mode = false,
+						download_remote_images = true,
+						only_render_image_at_cursor = false,
+						filetypes = { "norg" },
+					},
+					html = {
+						enabled = true,
+					},
+					css = {
+						enabled = true,
+					},
+				},
+				max_height_window_percentage = 40,
+				editor_only_render_when_focused = true,
+				tmux_show_only_in_active_window = true,
+			})
+		end,
+	},
+}
