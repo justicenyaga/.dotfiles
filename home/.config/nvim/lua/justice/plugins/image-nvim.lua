c43df1aa75140592e13d5bd08b2752cbcad3f5e3
Justice Nyaga <justiceevansmwema@gmail.com>
2024-09-26 18:06:36 +0300

    [nvim] Separate luarocks plugin from image-nvim

--- home/.config/nvim/lua/justice/plugins/image-nvim.lua
+++ home/.config/nvim/lua/justice/plugins/image-nvim.lua
@@ -1,42 +1,33 @@
 return {
-	{
-		"vhyrro/luarocks.nvim",
-		priority = 1001,
-		opts = {
-			rocks = { "magick" },
-		},
-	},
-	{
-		"3rd/image.nvim",
-		dependencies = { "luarocks.nvim" },
-		config = function()
-			require("image").setup({
-				integrations = {
-					markdown = {
-						enabled = true,
-						clear_in_insert_mode = false,
-						download_remote_images = true,
-						only_render_image_at_cursor = true,
-						filetypes = { "markdown", "vimwiki" },
-					},
-					neorg = {
-						enabled = true,
-						clear_in_insert_mode = false,
-						download_remote_images = true,
-						only_render_image_at_cursor = false,
-						filetypes = { "norg" },
-					},
-					html = {
-						enabled = false,
-					},
-					css = {
-						enabled = false,
-					},
+	"3rd/image.nvim",
+	dependencies = { "luarocks.nvim" },
+	config = function()
+		require("image").setup({
+			integrations = {
+				markdown = {
+					enabled = true,
+					clear_in_insert_mode = false,
+					download_remote_images = true,
+					only_render_image_at_cursor = true,
+					filetypes = { "markdown", "vimwiki" },
 				},
-				max_height_window_percentage = 40,
-				editor_only_render_when_focused = true,
-				tmux_show_only_in_active_window = true,
-			})
-		end,
-	},
+				neorg = {
+					enabled = true,
+					clear_in_insert_mode = false,
+					download_remote_images = true,
+					only_render_image_at_cursor = false,
+					filetypes = { "norg" },
+				},
+				html = {
+					enabled = false,
+				},
+				css = {
+					enabled = false,
+				},
+			},
+			max_height_window_percentage = 40,
+			editor_only_render_when_focused = true,
+			tmux_show_only_in_active_window = true,
+		})
+	end,
 }
