a5bf9a61cc116d593f7e05abcb8d372cbfa08c40
Justice Nyaga <justiceevansmwema@gmail.com>
2024-09-27 12:44:08 +0300

    [nvim] Enable spell checking

--- home/.config/nvim/lua/justice/core/options.lua
+++ home/.config/nvim/lua/justice/core/options.lua
@@ -13,6 +13,10 @@ opt.shiftwidth = 2 -- 2 spaces for indent width
 opt.expandtab = true -- expand tab to spaces
 opt.autoindent = true -- copy indent from current line when starting new one
 
+-- spell checking
+opt.spell = true -- turn on spell checking
+opt.spelllang = "en_us" -- sets the spell check language to English (US)
+
 -- line wrapping
 opt.wrap = false -- disable line wrapping
 
