0f6757df1518c239a95aee0a4f6bd77f191b2b51
Justice Nyaga <justiceevansmwema@gmail.com>
2024-10-16 11:51:16 +0300

    [nvim] Autoselect first cmp suggestion

--- home/.config/nvim/lua/justice/plugins/nvim-cmp.lua
+++ home/.config/nvim/lua/justice/plugins/nvim-cmp.lua
@@ -35,7 +35,7 @@ return {
 				},
 			},
 			completion = {
-				completeopt = "menu,menuone,preview,noselect",
+				completeopt = "menu,menuone,preview,noinsert",
 			},
 			snippet = { -- configure how nvim-cmp interacts with snippet engine
 				expand = function(args)
