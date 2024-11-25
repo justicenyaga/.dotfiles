47c371b6eaee10e389ae0a7e15c1e57ddb8e3c65
Justice Nyaga <justiceevansmwema@gmail.com>
2024-11-25 08:55:33 +0300

    [nvim] Use clip.exe on wsl

--- home/.config/nvim/lua/justice/core/options.lua
+++ home/.config/nvim/lua/justice/core/options.lua
@@ -59,20 +59,21 @@ opt.backspace = "indent,eol,start" -- allow backspace on indent, end of line or 
 -- clipboard
 opt.clipboard = "unnamedplus" -- use system clipboard as default register
 
--- IF ON WSL, UNCOMMENT THIS BLOCK TO INTERACT WITH WINDOWS CLIPBOARD
-
--- vim.g.clipboard = {
---   name = "win32yank-wsl",
---   copy = {
---     ["+"] = "win32yank.exe -i --crlf",
---     ["*"] = "win32yank.exe -i --crlf",
---   },
---   paste = {
---     ["+"] = "win32yank.exe -o --lf",
---     ["*"] = "win32yank.exe -o --lf",
---   },
---   cache_enabled = true,
--- }
+-- Use clip on wsl
+if vim.fn.has("wsl") == 1 then
+	vim.g.clipboard = {
+		name = "WslClipboard",
+		copy = {
+			["+"] = "clip.exe",
+			["*"] = "clip.exe",
+		},
+		paste = {
+			["+"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+			["*"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+		},
+		cache_enabled = 0,
+	}
+end
 
 -- split windows
 opt.splitright = true -- split vertical window to the right
