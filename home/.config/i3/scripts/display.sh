bafb5333873009118d856b3ee128a8e59e2d8acc
Justice Nyaga <justiceevansmwema@gmail.com>
2025-06-15 13:53:51 +0300

    Update display configs

--- home/.config/i3/scripts/display.sh
+++ home/.config/i3/scripts/display.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
 if xrandr | grep "HDMI-1 connected"; then
-    xrandr --output eDP-1 --mode 1366x768 --pos 1920x0 --rotate normal \
+    xrandr --output eDP-1 --mode 1920x1080 --pos 0x1080 --rotate normal \
            --output HDMI-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal \
-           --output DP-1 --off \
+           --output HDMI-2 --off
+elif xrandr | grep "^DP-1 connected"; then
+    xrandr --output eDP-1 --mode 1920x1080 --pos 0x1080 --rotate normal \
+           --output DP-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal \
            --output HDMI-2 --off
 else
-    xrandr --output eDP-1 --primary --mode 1366x768 --pos 0x0 --rotate normal \
+    xrandr --output eDP-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal \
            --output HDMI-1 --off \
            --output DP-1 --off \
            --output HDMI-2 --off
--- home/.config/waybar/launch.sh
+++ home/.config/waybar/launch.sh
@@ -2,9 +2,12 @@ killall waybar
 
 WAYBAR_CONFIG="$HOME/.config/waybar/config"
 HDMI_CONNECTED=$(xrandr | grep "HDMI-A-1 connected")
+DP_1_CONNECTED=$(xrandr | grep "^DP-1 connected")
 
 if [ -n "$HDMI_CONNECTED" ]; then
   OUTPUT="HDMI-A-1"
+elif [ -n "$DP_1_CONNECTED" ]; then
+  OUTPUT="DP-1"
 else
   OUTPUT="eDP-1"
 fi
