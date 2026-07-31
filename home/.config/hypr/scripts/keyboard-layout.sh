b9010773516c7d538e2577de623386cd209f8ab3
Justice Nyaga <justiceevansmwema@gmail.com>
2026-07-31 09:07:38 +0300

    [hypr] Update keyboard layout switcher to Colemak-DH

--- home/.config/hypr/scripts/keyboard-layout.sh
+++ home/.config/hypr/scripts/keyboard-layout.sh
@@ -2,10 +2,10 @@
 
 CURRENT=$(hyprctl getoption input:kb_variant | grep '^str:' | awk '{print $2}')
 
-if [ "$CURRENT" = "colemak" ]; then
+if [ "$CURRENT" = "colemak_dh" ]; then
     hyprctl keyword input:kb_variant ""
     notify-send "Keyboard" "Switched to QWERTY" -i input-keyboard -t 2000
 else
-    hyprctl keyword input:kb_variant "colemak"
-    notify-send "Keyboard" "Switched to Colemark" -i input-keyboard -t 2000
+    hyprctl keyword input:kb_variant "colemak_dh"
+    notify-send "Keyboard" "Switched to Colemak-DH" -i input-keyboard -t 2000
 fi
